d1d0090ae28b656c3b26d6c4ba016e065c0cdf74
pea10164268 <pea10164268@bridgwater.ac.uk>
2021-11-19 13:02:31 +0000

    Created React Tutorial

---
+++ ReactTutorial.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const App = () => {
+    const [count, setCount] = useState(0);
+return (
+    <View style = {styles.container}>
+        <Text>You clicked {count} times</Text>
+        <Button>
+            onPress={() => setCount(count + 1)}
+            title="Click me!"
+        </Button>
+</View>
+    );
+
+};
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
